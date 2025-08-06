85a4a86e127214a47b7530dd03f2b12c9aa77ea8
ashvini-chavan <37872268+ashvini-chavan@users.noreply.github.com>
2025-08-06 19:42:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,11 +14,11 @@ selected_quarter = variables['selected_quarter']
 qbr_year = variables['qbr_year']
 
 # Set Confluence URL and space key
-confluence_url = ''
-space_key = ''
+confluence_url = 'https://ashviniaws.atlassian.net/wiki'
+space_key = 'testspace'
 
 # Fixed parent page ID under which all pages will be created
-parent_page_id = ''
+parent_page_id = '851994'
 
 # Initialize Confluence client
 confluence = Confluence(
@@ -35,10 +35,10 @@ for html_file in html_files:
     page_title = os.path.splitext(html_file)[0]  # Remove .html extension
     with open(html_file, 'r') as f:
         html_content = f.read()
-    
+
     # Check if child page exists under the space
     existing_page = confluence.get_page_by_title(space=space_key, title=page_title)
-    
+
     if existing_page:
         # Update existing page
         confluence.update_page(
